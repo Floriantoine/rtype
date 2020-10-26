7d91d12026ae7d9d7d0b773d375363f962062923
F <fahad.assoumani@epitech.eu>
2020-10-26 17:01:18 +0100

    [SETUP] conan deps and cmakefiles to build both server and client [ADD] gitignore + startup script

---
+++ client/src/main.cpp
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2020
+** client
+** File description:
+** main
+*/
+
+#include <iostream>
+
+int main(void)
+{
+    return 0;
+}
---
+++ server/src/main.cpp
@@ -0,0 +1,14 @@
+/*
+** EPITECH PROJECT, 2020
+** B-CPP-501-BDX-5-1-rtype-albert.corson
+** File description:
+** main
+*/
+
+#include <iostream>
+#include <boost/asio.hpp>
+
+int main(void)
+{
+    return 0;
+}
