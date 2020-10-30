b5a3edddbd9395a6fb0e1ceb7a9cb666f8f5cf75
Albert Corson <albert.corson@epitech.eu>
2020-10-30 14:37:06 +0100

    [ADD] conanprofile using compiler.libcxx=libstdc++11

--- build.sh
+++ build.sh
@@ -1,6 +1,11 @@
+#!/bin/bash
+
+WORKDIR="$(dirname "$(readlink -f "$0")")"
+CONAN_PROFILE="$WORKDIR/conanprofile"
+
 rm -rf build && \
 mkdir build && \
 cd build && \
-conan install .. --build=missing && \
+conan install "$WORKDIR" --build=missing --profile="$CONAN_PROFILE" && \
 cmake .. -G "Unix Makefiles" && \
 cmake --build .
