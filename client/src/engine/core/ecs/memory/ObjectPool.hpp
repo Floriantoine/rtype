aab2330399a2eb05b9b35acbfd1d9ea3d24f631c
Adrien Lucbert <adrien.lucbert@epitech.eu>
2020-11-01 23:24:59 +0100

    [FEAT] ObjectPool can now grow when full

--- client/src/engine/core/ecs/memory/ObjectPool.hpp
+++ client/src/engine/core/ecs/memory/ObjectPool.hpp
@@ -1,16 +1,67 @@
 #pragma once
 
-#include <iostream>
 #include <cstddef>
 #include <vector>
-
-#include "Poolable.hpp"
+#include <numeric>
+#include <stdexcept>
 
 template<class T, std::size_t chunkSize = 1024>
 class ObjectPool
 {
     private:
-        std::vector<Poolable<T>> _poolables;
+        class Chunk
+        {
+            private:
+                std::vector<std::size_t> unusedIds_;
+                std::vector<T> objects_;
+
+            public:
+                Chunk()
+                    : unusedIds_ { std::vector<std::size_t>(chunkSize) }
+                    , objects_ { std::vector<T>(chunkSize) }
+                {
+                    std::iota(this->unusedIds_.begin(), this->unusedIds_.end(), 0);
+                }
+
+                bool isFull() const
+                {
+                    return this->unusedIds_.size() == 0;
+                }
+
+                T *get()
+                {
+                    if (isFull()) {
+                        throw std::out_of_range("Cannot get object from a full chunk");
+                    }
+                    std::size_t id = this->unusedIds_.back();
+                    this->unusedIds_.pop_back();
+                    return &this->objects_[id];
+                }
+
+                void release(T *ptr)
+                {
+                    std::size_t index = this->getObjectId(ptr);
+                    this->unusedIds_.push_back(index);
+                }
+
+                bool contains(T *ptr) const
+                {
+                    std::intptr_t regionStart = reinterpret_cast<std::intptr_t>(&this->objects_[0]);
+                    std::intptr_t regionSize = sizeof(T) * chunkSize;
+                    std::intptr_t regionEnd = regionStart + regionSize;
+                    std::intptr_t objectPtr = reinterpret_cast<std::intptr_t>(ptr);
+                    return (regionStart <= objectPtr && objectPtr < regionEnd);
+                }
+
+            private:
+                std::size_t getObjectId(T *ptr) const
+                {
+                    ptrdiff_t index = ptr - &this->objects_[0];
+                    return index;
+                }
+        };
+
+        std::vector<Chunk> chunks_;
 
     public:
         ObjectPool() = default;
@@ -18,31 +69,29 @@ class ObjectPool
 
         T *get()
         {
-            std::size_t index = 0;
-
-            for (auto &it: _poolables) {
-                if (!it.isUsed) {
-                    it.isUsed = true;
-                    return &it.object;
+            for (auto &chunk: this->chunks_) {
+                if (!chunk.isFull()) {
+                    return chunk.get();
                 }
-                ++index;
             }
             this->grow();
-            _poolables[index].isUsed = true;
-            return &_poolables[index].object;
+            return this->chunks_.back().get();
         }
 
-        void release(T *ptr) const
+        void release(T *ptr)
         {
-            constexpr std::size_t offset = offsetof(Poolable<T>, object);
-            Poolable<T> *poolable = reinterpret_cast<Poolable<T> *>(ptr - offset);
-
-            poolable->isUsed = false;
+            for (auto &chunk: this->chunks_) {
+                if (chunk.contains(ptr)) {
+                    chunk.release(ptr);
+                    return;
+                }
+            }
+            throw std::invalid_argument("Cannot release object from this pool");
         }
 
     private:
         void grow()
         {
-            _poolables.resize(_poolables.size() + chunkSize);
+            this->chunks_.push_back(Chunk());
         }
 };
