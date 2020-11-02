efafebfdeff9317f115a4c7ef1b723d20075cec5
Adrien Lucbert <adrien.lucbert@epitech.eu>
2020-11-02 15:08:45 +0100

    [FEAT] improved ObjectPool constructor initialization and added IObjectPool

---
+++ client/src/engine/core/ecs/memory/IObjectPool.hpp
@@ -0,0 +1,9 @@
+/**
+ * ObjectPool interface, mainly used to store ObjectPool of different types 
+ * together
+ */
+class IObjectPool
+{
+    protected:
+        virtual ~IObjectPool() = default;
+};
