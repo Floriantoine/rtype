244bf4bf3ef5e7870ece6c5c8e10ec37d0487640
Albert Corson <albert.corson@epitech.eu>
2020-11-09 23:08:32 +0100

    [WIP] SceneLoader nearly complete (missing methods from other classes)

--- deps/scene_loader/SceneLoader.hpp
+++ deps/scene_loader/SceneLoader.hpp
@@ -21,33 +21,35 @@
 namespace rtype {
     class SceneLoader {
       public:
-        typedef std::function<std::shared_ptr<ComponentBase>(const nlohmann::json &json, const std::shared_ptr<ComponentBase> &)> component_factory_t;
+        typedef std::function<std::unique_ptr<ComponentBase>(const nlohmann::json &body)> component_factory_t;
 
-        class Exception : rtype::Exception {
+        class Exception : public rtype::Exception {
           public:
             Exception(const std::string &msg)
                 : rtype::Exception("SceneLoader exception: " + msg)
-            {
-            }
-
+            { }
             Exception(std::string &&msg)
                 : rtype::Exception("SceneLoader exception: " + std::move(msg))
-            {
-            }
+            { }
             virtual ~Exception() override = default;
         };
 
       private:
-        static std::unordered_map<std::string, component_factory_t> componentFactory_;
-
         struct EntityDefinition {
-            std::vector<std::shared_ptr<ComponentBase>> components;
+            std::unordered_map<std::string, nlohmann::json> components;
+
+            EntityDefinition &operator=(const EntityDefinition &other)
+            {
+                this->components = other.components;
+                return *this;
+            }
         };
 
-        bool good_ { false };
+        static std::unordered_map<std::string, component_factory_t> componentFactory_;
+
+        bool good_ { true };
         nlohmann::json json_;
         std::unordered_map<std::string, EntityDefinition> definitions_;
-        Scene::State state_;
 
         static const auto jsonAt_(const nlohmann::json &json, const std::string &key, const std::string &errorContext = "")
         {
@@ -61,23 +63,19 @@ namespace rtype {
         {
             const auto &typeJson = this->jsonAt_(body, "type", "for an object of type `component`");
             const std::string &type = typeJson->get<std::string>();
-            const auto &factory = SceneLoader::componentFactory_.find(type);
 
-            if (factory == SceneLoader::componentFactory_.cend()) {
-                throw Exception("no factory defined for `" + type + "` component");
-            }
-            def.components.emplace_back(std::move(factory->second(body, nullptr)));
+            def.components[typeJson->get<std::string>()] = body;
         }
 
-        const auto getDefinition_(const std::string &key, bool throwOnError = true)
+        const auto getDefinition_(const std::string &key, bool throwOnError = true) const
         {
-            const auto &def = this->definitions_.find(key);
-            if (throwOnError && def != this->definitions_.end())
+            const auto def = this->definitions_.find(key);
+            if (throwOnError && def == this->definitions_.end())
                 throw Exception("no defintion found for " + key);
             return (def);
         }
 
-        void loadState_()
+        void loadState_(Scene &scene) const
         {
             static const std::unordered_map<std::string, Scene::State> states = {
                 { "INACTIVE", Scene::STATE_INACTIVE },
@@ -92,13 +90,18 @@ namespace rtype {
             if (state == states.cend()) {
                 throw Exception("value `" + stateStr + "` for `state` isn't valid");
             }
-            this->state_ = state->second;
+            scene.setState(state->second);
         }
 
         void loadDefinitions_()
         {
-            auto defs = this->json_.find("definitions");
+            static bool loaded = false;
 
+            if (loaded)
+                return;
+            loaded = true;
+
+            auto defs = this->json_.find("definitions");
             for (const auto &it : *defs) {
                 const auto &id = this->jsonAt_(it, "id", "for an object of type `definition`")->get<std::string>();
                 const auto &check = this->getDefinition_(id, false);
@@ -116,6 +119,39 @@ namespace rtype {
             }
         }
 
+        void loadComponents_(EntityDefinition &entity, const nlohmann::json &json) const
+        {
+            for (const auto &component : json) {
+                std::string type = this->jsonAt_(json, "type")->get<std::string>();
+                entity.components[type].merge_patch(json);
+            }
+        }
+
+        void loadEntity_(Scene &scene, const nlohmann::json &json) const
+        {
+            const auto &baseJson = json.find("base");
+            EntityDefinition builder;
+
+            if (baseJson != json.end()) {
+                builder = this->getDefinition_(baseJson->get<std::string>())->second;
+            }
+
+            const auto &components = json.find("components");
+            if (components != json.end()) {
+                this->loadComponents_(builder, *components);
+            }
+
+            std::shared_ptr<Entity> entity = scene.createEntity().lock();
+            for (const auto &it : builder.components) {
+                const auto &factory = SceneLoader::componentFactory_.find(it.first);
+
+                if (factory == SceneLoader::componentFactory_.cend()) {
+                    throw Exception("no factory defined for `" + it.first + "` component");
+                }
+                // entity.addComponent { this->componentFactory_[it.first](it.second) };
+            }
+        }
+
       public:
         ~SceneLoader() = default;
 
@@ -130,9 +166,6 @@ namespace rtype {
             this->json_ = nlohmann::json::parse(fd, nullptr, false, true);
             if (this->json_ == nlohmann::json::value_t::discarded) {
                 this->good_ = false;
-            } else {
-                this->loadState_();
-                this->loadDefinitions_();
             }
         }
 
@@ -141,26 +174,20 @@ namespace rtype {
             SceneLoader::componentFactory_[type] = factory;
         }
 
-        std::unique_ptr<Scene> load() const
+        std::unique_ptr<Scene> load()
         {
+            if (!this->good_)
+                throw Exception("parsing error");
+
             std::unique_ptr scenePtr = std::make_unique<Scene>();
 
-            scenePtr->setState(this->state_);
+            this->loadDefinitions_();
+            this->loadState_(*scenePtr);
 
             const auto &json = this->jsonAt_(this->json_, "entities");
 
             for (const auto &it : *json) {
-                const auto &baseJson = it.find("base");
-                if (baseJson != it.end()) {
-                    // load template components
-                }
-                // create entity
-                // for each component in json {
-                //     call component factory with template if any
-                //     add component to entity
-                // }
-                // /!\ don't forget to add components that are defined only in the template and not in the JSON
-                //     (because the previous loop won't take them into account)
+                this->loadEntity_(*scenePtr, it);
             }
             return scenePtr;
         }
