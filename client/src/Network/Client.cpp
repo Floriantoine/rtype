a8b953cea51ee4ee7071f2343532e64849faf8a3
F <fahad.assoumani@epitech.eu>
2020-11-11 22:58:58 +0100

    [UPDATE] simplify Client managment by using a template class that specifi the protocol

---
+++ client/src/Network/Client.cpp
@@ -0,0 +1,78 @@
+/*
+** EPITECH PROJECT, 2020
+** client
+** File description:
+** Client
+*/
+
+#include "Client.hpp"
+
+
+rtype::Network::TcpClient::TcpClient(boost::asio::io_context &io_context, const std::string_view &adress, std::uint16_t port)
+    : io_context_(io_context), serv_socket_(io_context), endpoint_(boost::asio::ip::make_address(adress), port)
+{
+    std::cout << "Start Tcp Client" << std::endl;
+    try {
+        this->start();
+    } catch (std::exception &e) {
+        std::cerr << e.what() << std::endl;
+    }
+}
+
+void rtype::Network::TcpClient::start()
+{
+    this->serv_socket_.connect(this->endpoint_);
+}
+
+void rtype::Network::TcpClient::send(const BPC::Buffer &buffer)
+{
+    this->serv_socket_.write_some(boost::asio::buffer(buffer));
+}
+
+// TODO: change the corpse... see With Albert
+std::size_t parse_header(const boost::asio::streambuf &)
+{
+    return 42;
+}
+
+BPC::Buffer &rtype::Network::TcpClient::recv(std::vector<unsigned char> &recbuf)
+{
+    // Read the header
+    int header_size = 42; // TODO: This should be a macro
+    recbuf.resize(header_size);
+
+    auto bytes = this->serv_socket_.read_some(boost::asio::buffer(recbuf));
+    std::cout << "READ: " << bytes << " bytes from socket" << std::endl;
+
+    // Extract the size of the body
+    //auto body_size = parse_header(recbuf);
+    //recbuf.resize(body_size);
+    //boost::asio::read(this->serv_socket_, boost::asio::buffer(recbuf));
+    return recbuf;
+};
+
+rtype::Network::UdpClient::UdpClient(boost::asio::io_context &io_context, const std::string_view &adress, std::uint16_t port)
+    : io_context_(io_context), resolver_(io_context), socket_(io_context, udp::endpoint(udp::v4(), 0))
+{
+    std::cout << "Start Udp Client" << std::endl;
+    this->serv_endpoints = *this->resolver_.resolve(udp::v4(), adress, std::to_string(port));
+}
+
+void rtype::Network::UdpClient::send(const BPC::Buffer &buffer)
+{ 
+    socket_.send_to(boost::asio::buffer(buffer), this->serv_endpoints);
+}
+
+BPC::Buffer &rtype::Network::UdpClient::recv(std::vector<unsigned char> &recbuf)
+{
+    // Read the header
+    int header_size = 8;
+    recbuf.resize(header_size);
+
+    std::cout << "mes couilles" << std::endl;
+    this->socket_.receive_from(boost::asio::buffer(recbuf), this->serv_endpoints);
+    std::cout << "sur ton front" << std::endl;
+
+    // Extract the size of the body
+    return recbuf;
+};
--- client/src/main.cpp
+++ client/src/main.cpp
@@ -5,15 +5,17 @@
 ** Client main file
 */
 
+#include "BinaryProtocolCommunication.hpp"
+
+#include "Client.hpp"
+
+#include <boost/asio.hpp>
 #include <boost/asio/buffer.hpp>
 #include <boost/asio/write.hpp>
 #include <iostream>
-#include <boost/asio.hpp>
 #include <sstream>
-#include "BinaryProtocolCommunication.hpp"
 
 namespace io = boost::asio;
-namespace BPC = BinaryProtocolCommunication;
 namespace ip = io::ip;
 using tcp = io::ip::tcp;
 using error_code = boost::system::error_code;
@@ -22,31 +24,12 @@ int main()
 {
     auto cm = BPC::CommunicationManager::Get();
     io::io_context io_context;
-    tcp::socket socket(io_context);
-    ip::address address = ip::make_address("127.0.0.1");
-    tcp::endpoint endpoint(address, 4219);
+
+    rtype::Network::IOClient<rtype::Network::UdpClient> client(io_context, "127.0.0.1", 4219);
+
     auto buffer = cm.serialize(BPC::BaseType::REQUEST, BPC::Method::CREATE);
-    std::array<char, 1080> abuffer;
-
-    error_code error;
-    socket.connect(endpoint, error);
-
-    if(!error) {
-        std::cout << "The connection has been established!";
-    } else {
-        std::cerr << "Something went wrong :(: " + error.message() << std::endl;
-    }
-    std::cout << "Writing " << socket.write_some(boost::asio::buffer(buffer)) << " datas to server" << std::endl;
-    std::cout << "Reading " << socket.read_some(boost::asio::buffer(abuffer)) << " datas to server" << std::endl;
-    //std::stringstream tmp;
-    //tmp << std::istream(&abuffer).rdbuf();
-    //abuffer.consume(1080);
-    //auto str = tmp.str();
-    BPC::Buffer bbuffer(abuffer.begin(), abuffer.end());
-    cm.deserialize(bbuffer);
-
-    
-
-    while (true);
+    client.write(buffer);
+    auto rec = client.read();
+    cm.deserialize(rec);
     return 0;
 }
